fe943fc2448868a24dac7275c31beb081c17c09d
1947860@uj.edu.sa <1947860@uj.edu.sa>
2023-05-11 13:30:31 +0300

    adding more info

--- Panda.py
+++ Panda.py
@@ -7,10 +7,17 @@ class bears:
         self.species = species
         self.type = type
 
+
+class panda (bears):
+    def __init__(self, wight: int, hight: int, species: str, type: str,made_of:str,avalible:str):
+        super().__init__(wight, hight, species, type)
+        self.made_of = made_of
+        self.avaliable = avalible
+
     def Types_of_bear(self):
-        return f"bear wight {self.wight} kg ,and thier hight usually {self.hight}, and I species  {self.species} of bears, and his type is {self.type}"
+        return f"bear wight {self.wight} kg ,and thier hight usually {self.hight}, and I species  {self.species} of bears, and his type is {self.type}, and he  is {self.made_of}, and until know he is {self.avaliable} "
 
-panda = bears (60,1.9,"bears","danager animals")
+panda1 = panda(100,80,"panda","dangrous","live anamals","exisit")
 
-print(panda.Types_of_bear())
+print(panda1.Types_of_bear())
 
