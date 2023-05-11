4f4a99cf32fcdf8d3909dcd9acfd26bc956c51bd
1947860@uj.edu.sa <1947860@uj.edu.sa>
2023-05-11 13:40:52 +0300

    Fixing a bit

--- Panda.py
+++ Panda.py
@@ -17,7 +17,18 @@ class panda (bears):
     def Types_of_bear(self):
         return f"bear wight {self.wight} kg ,and thier hight usually {self.hight}, and I species  {self.species} of bears, and his type is {self.type}, and he  is {self.made_of}, and until know he is {self.avaliable} "
 
-panda1 = panda(100,80,"panda","dangrous","live anamals","exisit")
+panda1 = panda(100,70,"panda","dangrous","live anamals","exisit")
+panda2 = panda(90,80,"black bear","dangrous","live anamals","exisit")
+panda3 = panda(80,90,"brown bear","dangrous","live anamals","exisit")
+panda4 = panda(70,100,"green bear","dangrous","live anamals","exisit")
+
 
 print(panda1.Types_of_bear())
+print("--"*16)
+print(panda2.Types_of_bear())
+print("--"*16)
+print(panda3.Types_of_bear())
+print("--"*16)
+print(panda4.Types_of_bear())
+
 
